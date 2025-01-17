c099736d87036241684371f17fcb0d7ec5dbbf41
Shubham53Mishra <21cse53@vmkvec.edu.in>
2025-01-17 17:38:00 +0530

    add the api

--- frontend/src/components/Job/JobDetails.jsx
+++ frontend/src/components/Job/JobDetails.jsx
@@ -12,7 +12,7 @@ const JobDetails = () => {
 
   useEffect(() => {
     axios
-      .get(`http://localhost:4000/api/v1/job/${id}`, {
+      .get(`https://mern-job-seeking-webapp-vmj1.onrender.com/api/v1/job/${id}`, {
         withCredentials: true,
       })
       .then((res) => {
